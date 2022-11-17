263d3a58720a7bf5c9dace208cc16a6159d8d4d4
Rowdy VdS <11782118+Rowdyvds@users.noreply.github.com>
2022-11-17 20:04:03 +0100

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //Check if both numbers are in fact numbers
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
+        return "ERROR";
+    }
+    
+    //Check if both numbers are positive
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    //Check if number 2 is the lowest number, if so, swap them
+    if (num2 < num1) {
+        [num1, num2] = [num2, num1];
+    }
+
+    //loop through all numbers and add to the sum
+    let sum = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
