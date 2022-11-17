e9bc5775123072dbd08ce3eae961f19189c9e035
Rowdy VdS <11782118+Rowdyvds@users.noreply.github.com>
2022-11-17 20:23:36 +0100

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5 / 9);
+  c = +(c.toFixed(1));
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * 1.8) + 32;
+  f = (+f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
