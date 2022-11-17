7e0fa7f2dbfb871c74d87ee39b7776c369cb7a89
Rowdy VdS <11782118+Rowdyvds@users.noreply.github.com>
2022-11-17 19:54:05 +0100

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    if (args.length == 0) {
+        return "ERROR: no arguments given";
+    } else if (args.length == 1) {
+        return args[0];
+    } else {
+        for (let i = 1; i < args.length; i++) {
+            for (let j = args[0].length - 1; j >=0; j--) {
+                if (args[i] === args[0][j]) {
+                    args[0].splice(j, 1);
+                }
+            }
+        }
+        return args[0];
+    }
 };
 
 // Do not edit below this line
